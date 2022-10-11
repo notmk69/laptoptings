20fdbb5a14401cb65008124ea4282badf6849b7b
notmk69 <munimkhaliq69@gmail.com>
2022-10-11 23:48:21 +0500

    Update yaniv.cpp

---
+++ yaniv.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    struct card{
+	            int suit,value;
+                };
+    card deck[52];
+    for(int i=0; i<52;i++)
+    {
+    	cards[i].suit=i%4;
+    	cards[i].value=i%13;
+    	
+	}
+
+
+
+
+
+
+
+
+
+}
