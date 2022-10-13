a97419f94135bf20ac580a1d37059addbf3f31e8
notmk69 <munimkhaliq69@gmail.com>
2022-10-13 10:20:37 +0500

    Create Untitled1.cpp

---
+++ Untitled1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int deck[52]={1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10};
+int p1[5],p2[5],p3[5],p4[5],index=0,open;
+int main()
+{
+	for(int i=1; i<5; i++)
+	{
+		p1[i]=cards[index];
+		index++;
+		p2[i]=cards[index];
+		index++;
+		p3[i]=cards[index];
+		index++
+		p4[i]=cards[index];
+		index++;
+		
+	
+		
+		
+	}
+	
+		open=cards[index];
+		index++;
+		
+	
+	
+}
