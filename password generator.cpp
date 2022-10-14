9ba421811176a94a69d490631bcde6dfbae947e0
notmk69 <munimkhaliq69@gmail.com>
2022-10-14 21:01:35 +0500

    gud

---
+++ password generator.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+string in;
+string name;
+string user;
+int x,c;
+string y;
+string password="";
+char pass[75]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+               'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+               '1','2','3','4','5','6','7','8','9','0',' ','!','@','#','$','%','^','&','*','(',')'};
+
+
+int main()
+{
+	cout<<"*********************************PASSSWORD GENERATOR************************************ \n";
+	cout<<"press 1 to generate a password  \npress 2 to view existing passwords \n";
+	cin>>c;
+	if (c==1)
+	{
+	cout<<"please enter website or application name \n";
+	cin>>name;
+	cout<<"please enter the username for the password \n";
+	cin>>user;
+	srand(time(NULL));
+
+	
+	for (int i=1; i<=16 ;i++ )
+	{
+		x=rand()%72+1;
+		password=password+pass[x];
+	}
+	cout<<"the random password is :  "<<password<<endl;
+	
+	
+	
+	
+	fstream passes;
+	passes.open("xyz.txt",ios::out|ios::app|ios::in);
+	passes<<name<<" "<<user<<":"<<password<<endl;
+	cout<<"password saved successfully \n";
+	passes.close();
+	
+	
+//    ifstream pass("xyz.txt");
+//        getline(pass,y);
+//        cout<<y;
+	
+	cout<<"press spacebar to copy to clipboard";
+	in = getch();
+	if (in==" ")
+	{
+	const char* output=password.c_str();
+    const size_t len = strlen(output) + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), output, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+}
+	else if (c==2)
+	{
+	ifstream read;
+	read.open("xyz.txt");
+	while(!read.eof())
+	{
+	getline(read,y);
+	cout<<y<<endl;
+}
+	}
+	return 0;
+	
+}
--- yaniv.cpp
+++ yaniv.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 
 int cards[52]={1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10,1,2,3,4,5,6,7,8,9,10,0,10,10};
-int p1[5],p2[5],p3[5],p4[5],index=0,open,player=1;
+int p1[5],p2[5],p3[5],p4[5],open,player=1;
+int maxi=0,index=0;
+int max_index=0;
+int sum1,sum2,sum3,sum4,min_index,mini;
+
 int main()
 {
 	while (index<52)
 	{
-	int max=0,max_index=0;
-	for(int i=1; i<5; i++)
+	
+	for(int i=1; i<6; i++)
 	{
 		p1[i]=cards[index];
 		index++;
@@ -18,10 +22,14 @@ int main()
 		p3[i]=cards[index];
 		index++;
 		p4[i]=cards[index];
-		index++;
+		index++;cout<<index<<endl;
 	}
 		open=cards[index];
 		index++;
+
+		
+			
+	
 	while(index<52)
 	{
 	    if (player==1)
@@ -29,14 +37,14 @@ int main()
 			max_index=0;
 			for(int i=0 ; i<5;i++)
 			{
-				if (max<p1[i])
+				if (maxi<p1[i])
 				{
 				
-					max=p1[i];
+					maxi=p1[i];
 				    max_index=i;
                 }
 			}
-			if (max>open)
+			if (maxi>open)
 			{
 				swap(open,p1[max_index]);
 			}
@@ -45,7 +53,7 @@ int main()
 				open=cards[index];
 				index++;
 			}
-			if(max>open)
+			if(maxi>open)
 			{
 				swap(open,p1[max_index]);
 			}
@@ -61,14 +69,14 @@ int main()
 				max_index=0;
 			for(int i=0 ; i<5;i++)
 			{
-				if (max<p2[i])
+				if (maxi<p2[i])
 				{
 				
-					max=p2[i];
+					maxi=p2[i];
 				    max_index=i;
                 }
 			}
-			if (max>open)
+			if (maxi>open)
 			{
 				swap(open,p2[max_index]);
 			}
@@ -77,7 +85,7 @@ int main()
 				open=cards[index];
 				index++;
 			}
-			if(max>open)
+			if(maxi>open)
 			{
 				swap(open,p2[max_index]);
 			}
@@ -91,14 +99,14 @@ int main()
 				max_index=0;
 			for(int i=0 ; i<5;i++)
 			{
-				if (max<p3[i])
+				if (maxi<p3[i])
 				{
 				
-					max=p3[i];
+					maxi=p3[i];
 				    max_index=i;
                 }
 			}
-			if (max>open)
+			if (maxi>open)
 			{
 				swap(open,p3[max_index]);
 			}
@@ -107,7 +115,7 @@ int main()
 				open=cards[index];
 				index++;
 			}
-			if(max>open)
+			if(maxi>open)
 			{
 				swap(open,p3[max_index]);
 			}
@@ -122,14 +130,14 @@ int main()
 				max_index=0;
 			for(int i=0 ; i<5;i++)
 			{
-				if (max<p4[i])
+				if (maxi<p4[i])
 				{
 				
-					max=p4[i];
+					maxi=p4[i];
 				    max_index=i;
                 }
 			}
-			if (max>open)
+			if (maxi>open)
 			{
 				swap(open,p4[max_index]);
 			}
@@ -138,7 +146,7 @@ int main()
 				open=cards[index];
 				index++;
 			}
-			if(max>open)
+			if(maxi>open)
 			{
 				swap(open,p4[max_index]);
 			}
@@ -148,20 +156,46 @@ int main()
 		
 	}	
 	}
-	for(int i=0 ; i<5; i++)
-	    cout<<p1[i]<<"  ";
-	    cout<<endl;
-	    
-	    for(int i=0 ; i<5; i++)
-	    cout<<p2[i]<<"  ";
-	    cout<<endl;
-	    
-	    for(int i=0 ; i<5; i++)
-	    cout<<p3[i]<<"  ";
-	    cout<<endl;
-	    
-	    for(int i=0 ; i<5; i++)
-	    cout<<p4[i]<<"  ";
-	    cout<<endl;
-	
+//	for(int i=0 ; i<5; i++){
+//	
+//	    cout<<p1[i]<<"  ";
+//	    cout<<endl;
+//	    
+//	    for(int i=0 ; i<5; i++)
+//	    cout<<p2[i]<<"  ";
+//	    cout<<endl;
+//	    
+//	    for(int i=0 ; i<5; i++)
+//	    cout<<p3[i]<<"  ";
+//	    cout<<endl;
+//	    
+//	    for(int i=0 ; i<5; i++)
+//	    cout<<p4[i]<<"  ";
+//	    cout<<endl;
+//	}
+	    for(int i=0; i<5;i++)
+	    {
+	    	sum1=sum1+p1[i];
+	    	sum2=sum2+p2[i];
+	    	sum3=sum3+p3[i];
+	    	sum4=sum4+p4[i];
+		}
+	//	cout<<sum1<<"  "<<sum2<<"  "<<sum3<<"  "<<sum4<<endl;
+		int arey[4]={sum1,sum2,sum3,sum4};
+		mini=arey[0];
+		for(int i=0;i<5;i++)
+		{
+			if(mini>arey[i])
+			{
+				mini=arey[i];
+				min_index=i;
+			}
+			
+		}
+	//	cout<<mini<<endl;
+
+		
+
+return 0;
 }
+
